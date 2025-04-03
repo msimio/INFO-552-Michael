34bdb17041b6c203ab7be36d061c7faddbec110c
msimio <137801161+msimio@users.noreply.github.com>
2025-04-02 19:49:56 -0500

    remove 5ms update

--- js/scripts.js
+++ js/scripts.js
@@ -111,7 +111,7 @@ function countSteps(event) {
           const computedStyle = getComputedStyle(progressBar)
           const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
           progressBar.style.setProperty('--width', width + .1)
-        }, 5)
+        })
 
     }
 }
